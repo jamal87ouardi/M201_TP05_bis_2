96c0c3eff2247a580d6c8be1ca7d7a1221c2ec87
unknown <ouardijamal87@gmail.com>
2023-09-22 09:15:15 +0100

    first

---
+++ app/src/main/java/com/example/m201_tp05_bis/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.m201_tp05_bis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Login incorrect",Toast.LENGTH_LONG).show();
+
+
+    }
+}
